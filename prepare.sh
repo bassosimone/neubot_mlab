cbb622430d18c1c8a4edd61eddb027f46be1723e
Simone Basso <bassosimone@gmail.com>
2013-03-06 22:41:59 +0100

    prepare.sh: merge changes after code review
    1. use sh instead of bash and propagate changes;
    
    2. make directory where to clone configurable;
    
    3. fetch from `stable_mlab` branch;
    
    4. arrange things so we don't need to clone each time;
    
    5. construct the tarball so the permissions of the remote
       home directory are not overriden when we unpack.

--- prepare.sh
+++ prepare.sh
@@ -1,28 +1,48 @@
-#!/bin/bash
+#!/bin/sh -e
 
-set -x
-set -e
+#
+# init/prepare.sh - Prepares the tarball that should be installed
+# in all Measurement Lab slivers.
+#
+# Written by Stephen Soltesz and Simone Basso.
+#
 
-TOPDIR=/home/mlab_neubot
-mkdir -p $TOPDIR
+GITDIR=neubot
+BUILDDIR=buildir
 
-rm -rf neubot
-[ -d neubot ] || \
-    git clone https://github.com/neubot/neubot.git
+[ -d $GITDIR ] || \
+    git clone https://github.com/neubot/neubot.git $GITDIR
 
-pushd neubot
+(
+    cd $GITDIR
     destdir=dist/mlab
     tarball=$destdir/neubot.tar.gz
     version=$destdir/version
 
     rm -rf -- $destdir
     mkdir -p $destdir
+    #
+    # Simone maintains a branch for deploying into M-Lab (and, yes, he
+    # writes comments using the third person :-P)
+    #
+    git checkout stable_mlab
+    git fetch origin
+    git reset --hard origin/stable_mlab
     git archive --format=tar --prefix=neubot/ HEAD|gzip -9 > $tarball
     git describe --tags > $version
-popd
+)
 
-cp neubot/dist/mlab/* $TOPDIR/
-cp -r init $TOPDIR/
+rm -rf $BUILDDIR
+mkdir -p $BUILDDIR/init
+cp initialize.sh start.sh stop.sh $BUILDDIR/init
+cp neubot/dist/mlab/* $BUILDDIR
 
-tar -C $TOPDIR -cvf mlab_neubot.tar .
-
+#
+# Spell the list of files we want to install at /home/mlab_neubot because
+# we don't want to include the current directory into the archive. If we
+# include it, in fact, and we unpack the archive as root, the ownership of
+# is steal by root -- as a consequenc the ordinary user `mlab_neubot` is
+# not able to write new files (unless he/she logs in and fixes the ownership,
+# which is annoying).
+#
+tar -C $BUILDDIR -cvf mlab_neubot.tar init neubot.tar.gz version
