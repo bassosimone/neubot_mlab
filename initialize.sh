0551be25a8fdc92763dfe974f429af2a7b2dcf52
Simone Basso <bassosimone@gmail.com>
2013-03-06 22:40:14 +0100

    initialize.sh: don't use /etc/rc.d/rc.local to startup neubot
    The idea is to use scripts installed in the home directory: stop
    assuming we have /etc/rc.d/rc.local instead.
    
    While there, make some cosmetic changes and add a few lines of
    explanation at the beginning.

--- initialize.sh
+++ initialize.sh
@@ -1,25 +1,31 @@
-#!/bin/bash
+#!/bin/sh -e
+
+#
+# init/initialize.sh - Prepares the environment for Neubot after it
+# has been installed on a sliver.
+#
+# Written by Stephen Soltesz and Simone Basso.
+#
 
 cd /home/mlab_neubot
-SUDO="/usr/bin/sudo"
 
-set -e
-set -x
-
-if [ -f neubot.tar.gz ] ; then 
+if [ -f neubot.tar.gz ]; then 
     echo "install neubot"
     tar -xzf neubot.tar.gz
     python -m compileall -q neubot/neubot/
 
     echo "start new neubot"
     /home/mlab_neubot/neubot/M-Lab/install.sh
-    /etc/rc.d/rc.local
+    /home/mlab_neubot/init/start.sh
 
     echo "cleanup"
     #rm -rf neubot.tar.gz
+
     echo "make sure we've bind all ports"
-    netstat -a --tcp -n | grep LISTEN | awk '{print $4}' | sort > neubot/M-Lab/ports.new
+    netstat -a --tcp -n | grep LISTEN | awk '{print $4}' \
+        | sort > neubot/M-Lab/ports.new
     diff -u neubot/M-Lab/ports.txt neubot/M-Lab/ports.new
+
 else
     echo "neubot.tar.gz missing"
     echo "really shouldn't happen, but you never know."
