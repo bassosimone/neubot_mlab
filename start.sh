152080a02e2a542eefb737a9003b4cf2e408ba96
Simone Basso <bassosimone@gmail.com>
2013-03-19 19:24:51 +0100

    start.sh stop.sh: check for privileges moved back to neubot/M-Lab

--- start.sh
+++ start.sh
@@ -28,10 +28,5 @@
 # Measurement Lab, the real startup script is bundled with Neubot sources.
 #
 
-if [ `id -u` -ne 0 ]; then
-    echo "$0: FATAL: need root privileges" 1>&2
-    exit 1
-fi
-
 DEBUG=
 $DEBUG /home/mlab_neubot/neubot/M-Lab/start.sh
